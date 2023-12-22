d761b42ad19397610d01f63106123b49e020b71e
Mariana Piz <marianapiz0412@gmail.com>
2023-12-22 15:31:00 +0200

    Initial commit

---
+++ src/main/java/com/mapthree/mentorizonserver/MentorizonServerApplication.java
@@ -0,0 +1,13 @@
+package com.mapthree.mentorizonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorizonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorizonServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapthree/mentorizonserver/MentorizonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapthree.mentorizonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorizonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
